25fc414f88ce2622781af551af07559f9a66eb77
JulioEstrela <julioantunesestrela13@gmail.com>
2024-11-18 17:58:01 -0300

    add: connection layer

---
+++ src/main/java/br/com/yokohama/seguros/connection/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.com.yokohama.seguros.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection conectar() {
+		try {
+			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "", "");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
