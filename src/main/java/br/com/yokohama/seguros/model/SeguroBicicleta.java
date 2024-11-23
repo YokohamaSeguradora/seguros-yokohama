aaf3428f2ee4290a57471f131d0d33e9070d8afa
Julio Estrela <julioantunesestrela13@gmail.com>
2024-11-23 17:00:46 -0300

    refactor: model layer - Seguro

--- src/main/java/br/com/yokohama/seguros/model/SeguroBicicleta.java
+++
@@ -1,9 +0,0 @@
-package br.com.yokohama.seguros.model;
-
-public class SeguroBicicleta extends Seguro {
-
-	public SeguroBicicleta() {
-		super(TipoSeguro.BICICLETA);
-	}
-
-}
--- src/main/java/br/com/yokohama/seguros/model/SeguroCaminhao.java
+++
@@ -1,9 +0,0 @@
-package br.com.yokohama.seguros.model;
-
-public class SeguroCaminhao extends Seguro {
-	
-	public SeguroCaminhao() {
-		super(TipoSeguro.CAMINHAO);
-	}
-	
-}
