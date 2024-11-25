67eb667810169375b89a4d92686209e34a7d053b
Thiago Temoteo <thiago.temoteo1@gmail.com>
2024-11-24 22:04:27 -0300

    feat: button close application on menu view

--- src/main/java/br/com/yokohama/seguros/view/MenuCliente.java
+++ src/main/java/br/com/yokohama/seguros/view/MenuCliente.java
@@ -20,6 +20,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.JInternalFrame;
 
 public class MenuCliente extends JFrame {
 
@@ -29,7 +30,7 @@ public class MenuCliente extends JFrame {
 	public static void main(String[] args) {
 		FlatLightLaf.setup();
 		UIManager.put("Button.arc", 20);
-		UIManager.put("Button.hoverForeground", Color.red);
+		UIManager.put("Button.hoverForeground", new Color(127, 11, 11));
 		UIManager.put("Button.Button.pressedForeground", Color.white);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -64,6 +65,39 @@ public class MenuCliente extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		JInternalFrame frameWarning = new JInternalFrame("New JInternalFrame");
+		frameWarning.getContentPane().setBackground(new Color(240, 240, 240));
+		frameWarning.setBounds(322, 105, 369, 208);
+		contentPane.add(frameWarning);
+		frameWarning.getContentPane().setLayout(null);
+
+		JButton btnNewButton = new JButton("Fechar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setBounds(201, 110, 104, 23);
+		frameWarning.getContentPane().add(btnNewButton);
+
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frameWarning.setVisible(false);
+			}
+		});
+		btnCancelar.setBackground(new Color(255, 255, 255));
+		btnCancelar.setBounds(48, 110, 104, 23);
+		frameWarning.getContentPane().add(btnCancelar);
+
+		JLabel labelMensagem = new JLabel("você deseja fechar o programa ?");
+		labelMensagem.setForeground(new Color(255, 0, 0));
+		labelMensagem.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelMensagem.setBounds(56, 39, 241, 37);
+		frameWarning.getContentPane().add(labelMensagem);
+		frameWarning.setVisible(false);
 
 		JLabel userIcone = new JLabel("New label");
 		userIcone.setIcon(new ImageIcon(carregaImagen("/images/userLogo.png")));
@@ -277,6 +311,20 @@ public class MenuCliente extends JFrame {
 		panel_7.setBackground(Color.BLACK);
 		panel_7.setBounds(29, 369, 20, 1);
 		panelMenu.add(panel_7);
+
+		JButton botaoVoltar = new JButton(" ");
+		botaoVoltar.setHorizontalAlignment(SwingConstants.RIGHT);
+		botaoVoltar.setBounds(-36, 535, 98, 37);
+		panelMenu.add(botaoVoltar);
+		botaoVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frameWarning.setVisible(true);
+			}
+		});
+		botaoVoltar.setIcon(new ImageIcon(carregaImagen("/images/ligar.png")));
+		botaoVoltar.setForeground(Color.WHITE);
+		botaoVoltar.setBorderPainted(false);
+		botaoVoltar.setBackground(new Color(127, 11, 11));
 
 		JButton menuLeft = new JButton("");
 		menuLeft.setEnabled(false);
